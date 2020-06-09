bf5c69690a2117fcdfdfbeb436a0abf033da1c4e
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-06-09 17:57:08 +0500

    fix typo in cart utilities

--- src/redux/cart/cartUtils.js
+++ src/redux/cart/cartUtils.js
@@ -16,12 +16,26 @@ export const removeItemFromCart = (itemToRemove, cartItems) => {
   );
 
   if (existingCartItem.quantity === 1) {
-    return cartItems.filter((item) => item.id === existingCartItem.id);
+    return cartItems.filter((item) => item.id !== existingCartItem.id);
   }
 
   return cartItems.map((item) =>
     item.id === itemToRemove.id
       ? { ...item, quantity: item.quantity - 1 }
       : item
+  );
+};
+
+export const calculateTotalPrice = (cartItems) => {
+  return cartItems.reduce(
+    (totalPrice, currentItem) => totalPrice + currentItem.price,
+    0
+  );
+};
+
+export const calculateTotalItems = (cartItems) => {
+  return cartItems.reduce(
+    (totalItems, currentItem) => totalItems + currentItem.quantity,
+    0
   );
 };
