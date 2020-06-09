9686ff3836a18abebcae7f0b176b6808c9e1a803
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-06-09 17:57:43 +0500

    add totalPrice and totalItems

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,7 +1,14 @@
-import { addItemToCart, removeItemFromCart } from "./cartUtils";
+import {
+  addItemToCart,
+  removeItemFromCart,
+  calculateTotalPrice,
+  calculateTotalItems,
+} from "./cartUtils";
 
 const INITIAL_STATE = {
   cartItems: [],
+  totalPrice: null,
+  totalItems: null,
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -10,11 +17,15 @@ export default (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(action.payload, state.cartItems),
+        totalPrice: calculateTotalPrice(state.cartItems),
+        totalItems: calculateTotalItems(state.cartItems),
       };
     case "REMOVE_ITEM":
       return {
         ...state,
         cartItems: removeItemFromCart(action.payload, state.cartItems),
+        totalPrice: calculateTotalPrice(state.cartItems),
+        totalItems: calculateTotalItems(state.cartItems),
       };
     default:
       return state;
