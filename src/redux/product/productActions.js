6b31d23e3dcea5d5dee2dd95858f075a625eb49e
4hmaad <51507194+4hmaad@users.noreply.github.com>
2020-06-17 18:06:42 +0500

    change products fetching from observable pattern to promise pattern

--- src/redux/product/productActions.js
+++ src/redux/product/productActions.js
@@ -1,21 +1,21 @@
 import { firestore } from "../../firebase/firebase.utils";
 
-export const fetchProducts = () => (dispatch) => {
+export const fetchProducts = () => async (dispatch) => {
   dispatch({ type: "FETCHING_PRODUCTS" });
 
   const productRef = firestore.collection("products");
 
-  productRef.onSnapshot((snapShot) => {
-    const productsData = snapShot.docs.map((doc) => {
-      const id = doc.id;
-      return {
-        id,
-        ...doc.data(),
-      };
-    });
+  const snapShot = await productRef.get();
 
-    dispatch({ type: "FETCHED_PRODUCTS", payload: productsData });
+  const productsData = snapShot.docs.map((doc) => {
+    const id = doc.id;
+    return {
+      id,
+      ...doc.data(),
+    };
   });
+
+  dispatch({ type: "FETCHED_PRODUCTS", payload: productsData });
 };
 
 export const addFilter = (category) => ({
